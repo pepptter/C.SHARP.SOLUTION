8a9191a99c75d8ceac4317f5f31f8956b17052e2
Petter Nyström <143556865+pepptter@users.noreply.github.com>
2023-12-30 22:18:36 +0100

    Delete Adressbook.WpfApp directory

--- Adressbook.WpfApp/App.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows;
-
-namespace WpfApp
-{
-    public partial class App : Application
-    {
-    }
-}
--- Adressbook.WpfApp/App.xaml.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows;
-
-namespace WpfApp
-{
-    public partial class App : Application
-    {
-    }
-}
